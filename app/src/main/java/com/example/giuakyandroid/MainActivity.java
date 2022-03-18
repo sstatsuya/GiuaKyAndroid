dde5f0b359389c1d635ee7846af28a1a8ef160da
LuongMinhTien <binpro113wer@gmail.com>
2022-03-18 10:38:12 +0700

    Tạo branch mới

--- app/src/main/java/com/example/giuakyandroid/MainActivity.java
+++ app/src/main/java/com/example/giuakyandroid/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "Hello tien", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Tạo ra brand mới", Toast.LENGTH_SHORT).show();
     }
 }
