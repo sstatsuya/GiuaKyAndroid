eaae23b4b6814462503c4c9c8fe9dee28e4ba15b
Thach Da <thachbovjp@gmail.com>
2022-03-26 11:12:26 +0700

    Fix activity khach hang, update them khach hang

--- app/src/main/java/ngocthach/KhachHangActivity.java
+++ app/src/main/java/ngocthach/KhachHangActivity.java
@@ -2,10 +2,12 @@ package ngocthach;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ListView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.giuakyandroid.R;
@@ -19,6 +21,8 @@ public class KhachHangActivity extends AppCompatActivity {
     ArrayList<KhachHang> DS_khachHangs = new ArrayList<>();
     ImageView iv_MoGD_ThemKH;
     ListView lv_DSKhachHang;
+    AdapterKhachHang adapterKhachHang;
+    public static final int REQUEST_TO_THEMKH = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,14 +47,14 @@ public class KhachHangActivity extends AppCompatActivity {
 
 
     private void setEvent() {
-        AdapterKhachHang adapterKhachHang = new AdapterKhachHang(this, R.layout.activity_items_khachhang,DS_khachHangs);
+        adapterKhachHang = new AdapterKhachHang(this, R.layout.activity_items_khachhang,DS_khachHangs);
         lv_DSKhachHang.setAdapter(adapterKhachHang);
 
         iv_MoGD_ThemKH.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(KhachHangActivity.this, ActivityThemKhachHang.class);
-                startActivity(intent);
+                startActivityForResult(intent,REQUEST_TO_THEMKH);
             }
         });
 
@@ -59,5 +63,20 @@ public class KhachHangActivity extends AppCompatActivity {
     private void setControl() {
         iv_MoGD_ThemKH = findViewById(R.id.iv_MoGD_ThemKH);
         lv_DSKhachHang = findViewById(R.id.lv_DSKhachHang);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if(requestCode == REQUEST_TO_THEMKH && resultCode == RESULT_OK){
+            KhachHang khachHang = intent.getParcelableExtra("khachhang");
+            if (khachHang != null){
+                int ID_AUTO = Integer.parseInt(DS_khachHangs.get(DS_khachHangs.size() - 1). getMaKH().replace("KH",""));
+                khachHang.setMaKH("KH" + ID_AUTO );
+                DS_khachHangs.add(khachHang);
+                adapterKhachHang.notifyDataSetChanged(); //Reload adapter
+            }
+
+        }
     }
 }
