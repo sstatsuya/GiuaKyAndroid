e7ef23052f0b9ceec001790ca75751fe12810956
Phạm Thạch <phamthach2506@gmail.com>
2022-03-31 15:28:21 +0700

    Thêm đơn hàng

---
+++ app/src/main/java/pthach/model/DonHang.java
@@ -0,0 +1,42 @@
+package pthach.model;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class DonHang implements Serializable {
+    String maDH;
+    LocalDate ngayDatHang;
+    String maKH;
+
+    public DonHang() {}
+
+    public DonHang(String maDH, LocalDate ngayDatHang, String maKH) {
+        this.maDH = maDH;
+        this.ngayDatHang = ngayDatHang;
+        this.maKH = maKH;
+    }
+
+    public String getMaDH() {
+        return maDH;
+    }
+
+    public void setMaDH(String maDH) {
+        this.maDH = maDH;
+    }
+
+    public LocalDate getNgayDatHang() {
+        return ngayDatHang;
+    }
+
+    public void setNgayDatHang(LocalDate ngayDatHang) {
+        this.ngayDatHang = ngayDatHang;
+    }
+
+    public String getMaKH() {
+        return maKH;
+    }
+
+    public void setMaKH(String maKH) {
+        this.maKH = maKH;
+    }
+}
