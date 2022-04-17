2c93bf0f025eb05250d61e0e7d7ec0dfd891035f
Phạm Thạch <phamthach2506@gmail.com>
2022-04-17 10:22:25 +0700

    update database connector class

--- app/src/main/java/database/DBSanPham.java
+++ app/src/main/java/database/DBSanPham.java
@@ -5,10 +5,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteStatement;
-<<<<<<< HEAD
 import android.util.Log;
-=======
->>>>>>> origin/PThach-Branch
 
 import androidx.annotation.Nullable;
 
@@ -16,7 +13,6 @@ import java.util.ArrayList;
 
 import database.model.SanPham;
 
-<<<<<<< HEAD
 public class DBSanPham {
     private static final String DATABASE_NAME = "GIUAKYANDROID";
     private static final int DATABASE_VERSION = 1;
@@ -33,70 +29,6 @@ public class DBSanPham {
         Log.i("Database", "DBSanPham.themDL " + sanPham.getMaSP() + " - " + sanPham.getTenSP());
         String sql = "insert into SANPHAM values(null,?,?,?,?)";
         SQLiteDatabase database = this.dbHelper.getWritableDatabase();
-=======
-public class DBSanPham extends SQLiteOpenHelper {
-    private static final String DATABASE_NAME = "GIUAKYANDROID";
-    private static final int DATABASE_VERSION = 1;
-
-    // SQL CREATE TABLE SANPHAM
-    private static final String DBCREATE_SQLSANPHAM = "CREATE TABLE SANPHAM (" +
-            "    MASP    INTEGER PRIMARY KEY AUTOINCREMENT," +
-            "    TENSP   TEXT," +
-            "    XUATXU  TEXT," +
-            "    DONGIA  DOUBLE," +
-            "    HINHANH BLOB );" ;
-
-    // SQL CREATE TABLE KHACHHANG
-    private static final String DBCREATE_SQLKHACHHANG = "CREATE TABLE KHACHHANG (" +
-            "    MAKH    INTEGER PRIMARY KEY AUTOINCREMENT," +
-            "    HINHANH BLOB," +
-            "    TENKH   TEXT," +
-            "    DIACHI  TEXT," +
-            "    DIENTHOAI  TEXT" +
-            ");";
-    // SQL CREATE TABLE DONHANG
-    private static final String DBCREATE_SQLDONHANG = "CREATE TABLE DONHANG (" +
-            "    MADH   INTEGER PRIMARY KEY AUTOINCREMENT," +
-            "    NGAYDH DATE," +
-            "    MAKH           REFERENCES KHACHHANG (MAKH) ON DELETE CASCADE" +
-            "                                               ON UPDATE CASCADE" +
-            "                                               MATCH SIMPLE" +
-            ");";
-    // SQL CREATE TABLE THONGTINDATHANG
-    private static final String DBCREATE_SQLTHONGTINDATHANG = "CREATE TABLE THONGTINDATHANG (" +
-            "    MADH       INTEGER REFERENCES DONHANG (MADH) ON DELETE CASCADE" +
-            "                                                 ON UPDATE CASCADE" +
-            "                                                 MATCH SIMPLE," +
-            "    MASP       INTEGER REFERENCES SANPHAM (MASP) ON DELETE CASCADE" +
-            "                                                 ON UPDATE CASCADE," +
-            "    SOLUONGDAT INTEGER," +
-            "    PRIMARY KEY (MADH, MASP)" +
-            ");";
-
-    public DBSanPham(@Nullable Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
-    }
-
-    @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL(DBCREATE_SQLKHACHHANG);
-        sqLiteDatabase.execSQL(DBCREATE_SQLSANPHAM);
-        sqLiteDatabase.execSQL(DBCREATE_SQLDONHANG);
-        sqLiteDatabase.execSQL(DBCREATE_SQLTHONGTINDATHANG);
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        sqLiteDatabase.execSQL("Drop table if exists KHACHHANG" );
-        sqLiteDatabase.execSQL("Drop table if exists SANPHAM" );
-        sqLiteDatabase.execSQL("Drop table if exists DONHANG" );
-        sqLiteDatabase.execSQL("Drop table if exists THONGTINDATHANG" );
-    }
-
-    public void themDL(SanPham sanPham) { //Hàm Create
-        String sql = "insert into SANPHAM values(null,?,?,?,?)";
-        SQLiteDatabase database = getWritableDatabase();
->>>>>>> origin/PThach-Branch
         SQLiteStatement statement = database.compileStatement(sql);
         statement.bindString(1, sanPham.getTenSP());
         statement.bindString(2, sanPham.getXuatXu());
@@ -109,11 +41,7 @@ public class DBSanPham extends SQLiteOpenHelper {
     public ArrayList<SanPham> docDL() { //Hàm Read
         ArrayList<SanPham> data = new ArrayList<>();
         String sql = "select * from SANPHAM";
-<<<<<<< HEAD
         SQLiteDatabase database = this.dbHelper.getReadableDatabase();
-=======
-        SQLiteDatabase database = getReadableDatabase();
->>>>>>> origin/PThach-Branch
         Cursor cursor = database.rawQuery(sql, null);
         if (cursor.moveToFirst()) {
             do {
@@ -132,14 +60,9 @@ public class DBSanPham extends SQLiteOpenHelper {
     }
 
     public void suaDL(SanPham sanPham) { //Hàm Update
-<<<<<<< HEAD
         Log.i("Database", "DBSanPham.suaDL " + sanPham.getMaSP() + " - " + sanPham.getTenSP() + " - " + sanPham.getXuatXu() + " - " + sanPham.getDonGia());
         String sql = "update SANPHAM set TENSP=?, XUATXU=?, DONGIA=?, HINHANH=? where MASP="+sanPham.getMaSP();
         SQLiteDatabase database = this.dbHelper.getWritableDatabase();
-=======
-        String sql = "update SANPHAM set TENSP=?, XUATXU=?, DONGIA=?, HINHANH=? where MASP="+sanPham.getMaSP();
-        SQLiteDatabase database = getWritableDatabase();
->>>>>>> origin/PThach-Branch
         SQLiteStatement statement = database.compileStatement(sql);
         statement.bindString(1, sanPham.getTenSP());
         statement.bindString(2, sanPham.getXuatXu());
@@ -150,26 +73,20 @@ public class DBSanPham extends SQLiteOpenHelper {
     }
 
     public void xoaDL(SanPham sanPham) { //Hàm Delete
-<<<<<<< HEAD
         Log.i("Database", "DBSanPham.delete " + sanPham.getMaSP());
         String sql = "delete from SANPHAM where MASP="+sanPham.getMaSP();
         SQLiteDatabase database = this.dbHelper.getWritableDatabase();
-=======
-        String sql = "delete from SANPHAM where MASP="+sanPham.getMaSP();
-        SQLiteDatabase database = getWritableDatabase();
->>>>>>> origin/PThach-Branch
         SQLiteStatement statement = database.compileStatement(sql);
         statement.executeUpdateDelete();
         database.close();
     }
 
-<<<<<<< HEAD
-=======
     //Get all SanPham base on maSP
     public SanPham getSanPhamByMaSP(int maSP) { //Hàm Read
+        Log.i("Database", "DBSanPham.getSanphamByMaSP " + maSP);
         String sql = "select * from SANPHAM" +
         " WHERE MASP = " + String.valueOf(maSP);
-        SQLiteDatabase database = getReadableDatabase();
+        SQLiteDatabase database = this.dbHelper.getReadableDatabase();
         Cursor cursor = database.rawQuery(sql, null);
         if (cursor.moveToFirst()) {
             do {
@@ -186,5 +103,4 @@ public class DBSanPham extends SQLiteOpenHelper {
         cursor.close();
         return null;
     }
->>>>>>> origin/PThach-Branch
 }
